d05deb0235022d43792ad4ec9588d0055965709b
GangadharPrathap <23A91A0416@aec.edu.in>
2025-12-18 11:56:17 +0530

    sending the files frontend to backend

---
+++ src/frontendtobackend.js
@@ -0,0 +1,5 @@
+//  to install a new dependency, run: npm install <package_name>
+/*
+    Frontend to Backend Module
+    
+    */
--- src/main_index.js
+++ src/main_index.js
@@ -1,12 +1,16 @@
 
 import express from 'express';
 import studentsRouter from '../Routers/studentsRouter.js';
-
+import cors from 'cors';
 
 const app = express();
 app.use(express.json()); // to parse JSON bodies
+app.use(cors()); // to enable CORS
 
 app.use('/', studentsRouter);
+app.get('/users', (req, res) => {
+    res.send('Welcome to the Backend Server');
+});
 app.listen(7000, () => {
     console.log(`server is running on port ${7000}`);
 });
